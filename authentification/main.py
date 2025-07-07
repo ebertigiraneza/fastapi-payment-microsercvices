737258a1e561a581f0f14f7350c062cbc7b78bb4
ebertigiraneza <igiranezaeberto24@gmail.com>
2025-07-07 16:43:13 +0200

    Update test endpoint path to include '/auth/' for improved clarity

--- authentification/main.py
+++ authentification/main.py
@@ -90,7 +90,7 @@ async def register_user(user: UserCreate):
 async def read_users_me(current_user: User = Depends(get_current_user)):
     return current_user
 
-@app.get("/test")
+@app.get("/auth/test")
 async def test():
     return {"message": "Test successful"}
 
