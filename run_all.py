65e25a71bc788d0c360a44d2ee58b657fdc7a200
ebertigiraneza <igiranezaeberto24@gmail.com>
2025-07-07 08:49:39 +0200

    Refactor run_all.py to launch Uvicorn services using Gunicorn instead of Uvicorn directly

--- run_all.py
+++ run_all.py
@@ -1,13 +1,13 @@
 # run_all.py        
+ 
 import subprocess
 
-# Pour chaque service, lancez Uvicorn avec reload désactivé
 services = [
-    "uvicorn wallet.app:app --host 0.0.0.0 --port 8004 --reload=false",
-    "uvicorn authentification.app:app --host 0.0.0.0 --port 8005 --reload=false"
-    "uvicorn deposit.app:app --host 0.0.0.0 --port 8006 --reload=false",
-    "uvicorn withdraw.app:app --host 0.0.0.0 --port 8007 --reload=false"
+    "gunicorn wallet.app:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000",
+    "gunicorn authentification.app:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001",
+    "gunicorn deposit.app:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8002",
+    "gunicorn withdraw.app:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8003"
 ]
 
 for cmd in services:
-    subprocess.Popen(cmd, shell=True)        
+    subprocess.Popen(cmd, shell=True)           
