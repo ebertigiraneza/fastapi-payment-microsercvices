e1748ae38ccbe9e4820541a7b25cc4856b73ff44
ebertigiraneza <igiranezaeberto24@gmail.com>
2025-07-07 09:57:34 +0200

    Refactor run_all.py to integrate FastAPI main application and mount services

--- run_all.py
+++ run_all.py
@@ -15,4 +15,19 @@ services = [
 
 for app, port in services:
     p = Process(target=run_service, args=(app, port))
-    p.start()     
+    p.start()    
+
+from fastapi.middleware.wsgi import WSGIMiddleware
+from fastapi import FastAPI
+
+from wallet.main import app as wallet
+from authentification.main import app as authentification
+from deposit.main import app as deposit
+from withdraw.main import app as withdraw
+
+main_app = FastAPI()
+
+main_app.mount("/wallet", WSGIMiddleware(wallet))
+main_app.mount("/auth", WSGIMiddleware(authentification))
+main_app.mount("/deposit", WSGIMiddleware(deposit))
+main_app.mount("/withdraw", WSGIMiddleware(withdraw))
