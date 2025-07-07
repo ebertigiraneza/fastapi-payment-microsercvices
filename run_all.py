7b6d9dde3466752df4cc8d020f420a2c084a4499
ebertigiraneza <igiranezaeberto24@gmail.com>
2025-07-07 08:54:10 +0200

    Refactor run_all.py to launch services using Gunicorn with Uvicorn workers

--- run_all.py
+++ run_all.py
@@ -1,13 +1,18 @@
-# run_all.py        
- 
-import subprocess
+# run_all.py              
+    
+import uvicorn
+from multiprocessing import Process
+
+def run_service(app: str, port: int):
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=False, workers=1)
 
 services = [
-    "gunicorn wallet.app:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000",
-    "gunicorn authentification.app:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8001",
-    "gunicorn deposit.app:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8002",
-    "gunicorn withdraw.app:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8003"
+    ("wallet.app:app", 8000),
+    ("authentification.app:app", 8001),
+    ("deposit.app:app", 8002),
+    ("withdraw.app:app", 8003)
 ]
 
-for cmd in services:
-    subprocess.Popen(cmd, shell=True)           
+for app, port in services:
+    p = Process(target=run_service, args=(app, port))
+    p.start()     
